86eb20a04e55f669147f220fa0e4f5b664be6c54
Shanna <shannameow@gmail.com>
2021-04-09 20:19:55 -0400

    put together fetchPostsWithQuery action to receive queried posts

--- src/components/home.js
+++ src/components/home.js
@@ -3,15 +3,18 @@ import Logo from "./logo";
 import SearchBar from "./searchBar";
 import RecentPosts from "./recentPosts";
 
-export default class Home extends Component {
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
+class Home extends Component {
   handleSearchBarSubmit(query) {
-    console.log("trying to handle submit for query", query);
+    this.props.fetchPostsWithQuery(query);
     this.props.history.push("/results");
   }
 
   render() {
     return (
-      <div className="app">
+      <div>
         <div>
           <Logo />
           <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
@@ -21,3 +24,5 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect(null, actions)(Home);
--- src/components/results.js
+++ src/components/results.js
@@ -2,19 +2,25 @@ import React, { Component } from "react";
 import Logo from "./logo";
 import SearchBar from "./searchBar";
 
+import { connect } from "react-redux";
+import * as actions from "../actions";
+
 class Results extends Component {
   handleSearchBarSubmit(query) {
-    console.log(query);
+    this.props.fetchPostsWithQuery(query);
   }
 
   render() {
     return (
-      <div>
+      <div className="results">
         <Logo size={55} />
-        <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
+        <SearchBar
+          page="results"
+          onSubmit={(query) => this.handleSearchBarSubmit(query)}
+        />
       </div>
     );
   }
 }
 
-export default Results;
+export default connect(null, actions)(Results);
