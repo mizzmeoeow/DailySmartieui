9d5f84bbab045763b5561f41e7f9d5584e1591f4
Shanna <shannameow@gmail.com>
2021-04-11 10:58:23 -0400

    modified post component to render a result post and set up render links

--- src/components/post.js
+++ src/components/post.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
 
 class Post extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      height: 0,
+    };
+  }
+
   renderTopics() {
     let topics = this.props.associated_topics.map((topic, index) => {
       return (
@@ -12,13 +20,39 @@ class Post extends Component {
     return topics;
   }
 
+  renderLinks() {
+    let links = this.props.post_links.map((post_link, index) => {
+      return (
+        <div className="post-link" key={index}>
+          <div className="post-link__box"></div>
+          <div className="post-link__link">
+            <a href={post_link.link_url}>Useful Link #{index + 1}</a>
+          </div>
+        </div>
+      );
+    });
+    return links;
+  }
+
   render() {
-    return (
-      <li className="recent-posts">
-        <div className="recent-post__title">{this.props.title}</div>
-        <div className="recnt-post__topics">{this.renderTopics()}</div>
-      </li>
-    );
+    if (this.props.type == "recent") {
+      return (
+        <li className="recent-post">
+          <div className="recent-post__title">{this.props.title}</div>
+          <div className="recent-post__topics">{this.renderTopics()}</div>
+        </li>
+      );
+    } else if (this.props.type == "result") {
+      return (
+        <li className="result-post">
+          <div className="result-post__topics">{this.renderTopics()}</div>
+          <div className="result-post__title">
+            <a href={this.props.url_for_post}>{this.props.title}</a>
+          </div>
+          <div className="result-post__links">{this.renderLinks()}</div>
+        </li>
+      );
+    }
   }
 }
 
--- src/components/results.js
+++ src/components/results.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Logo from "./logo";
 import SearchBar from "./searchBar";
-import ResultsPosts from "./recentPosts";
+import ResultsPosts from "./resultsPosts";
 
 import { connect } from "react-redux";
 import * as actions from "../actions";
@@ -15,7 +15,10 @@ class Results extends Component {
     return (
       <div className="results">
         <Logo size={55} />
-        <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)} />
+        <SearchBar
+          page="results"
+          onSubmit={(query) => this.handleSearchBarSubmit(query)}
+        />
         <ResultsPosts />
       </div>
     );
