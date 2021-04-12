8baab395da7cd7d100baf5a328c3e2638e16a65a
Shanna <shannameow@gmail.com>
2021-04-12 18:18:17 -0400

    added no-content to post links are empty for post, and applied flex styles to post-links

--- src/components/post.js
+++ src/components/post.js
@@ -53,6 +53,10 @@ class Post extends Component {
         </div>
       );
     });
+
+    if (links == 0) {
+      return <div className="no-content">No post links</div>;
+    }
     return links;
   }
 
